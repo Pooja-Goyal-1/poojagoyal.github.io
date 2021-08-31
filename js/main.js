bb63093b9c6fd6e9257f3725444915216e114237
Pooja Goyal <85310587+Pooja-Goyal-1@users.noreply.github.com>
2021-08-31 14:51:55 +0530

    Responsive portfolio website

--- js/main.js
+++ js/main.js
@@ -12,4 +12,42 @@ $(document).ready(function(){
 
         return false;
     })
+
+    //owl-carousel
+    $('.site-main .projects-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive:{
+            0: {
+                item:1
+            },
+            544: {
+                items: 2
+            }
+        }
+    })
+
+    // sticky navigation menu
+
+    let nav_offset_top = $('.header_area').height() + 50;
+
+    function navbarFixed(){
+        if($('.header_area').length){
+            $(window).scroll(function(){
+                let scroll = $(window).scrollTop();
+                if(scroll>=nav_offset_top){
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                }
+                else{
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+            
+        }
+    }
+
+    navbarFixed();
+
 });
+
